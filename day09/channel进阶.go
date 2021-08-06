d125d5f2d0857cb61da7518e3a1866e45872c580
Lusheng <sheng.lu@mail.hypers.com>
2021-08-06 22:33:47 +0800

    add: channel 实现任务编排

--- day09/channel进阶.go
+++ day09/channel进阶.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+	ch4 := make(chan int)
+
+	go func() {
+		for {
+			fmt.Println("goroutine 1")
+			time.Sleep(1 * time.Second)
+			ch2 <- 1
+			<-ch1
+		}
+	}()
+	go func() {
+		for {
+			<-ch2
+			fmt.Println("goroutine 2")
+			time.Sleep(1 * time.Second)
+			ch3 <- 1
+		}
+	}()
+	go func() {
+		for {
+			<-ch3
+			fmt.Println("goroutine 3")
+			time.Sleep(1 * time.Second)
+			ch4 <- 1
+		}
+	}()
+	go func() {
+		for {
+			<-ch4
+			fmt.Println("goroutine 4")
+			time.Sleep(1 * time.Second)
+			ch1 <- 1
+		}
+	}()
+
+	select {}
 }
