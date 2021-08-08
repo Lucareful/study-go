0ed48100e48acc323c42daeff903a96f13946cc1
Lusheng <sheng.lu@mail.hypers.com>
2021-08-08 21:44:19 +0800

    update:chan or_done 任务编排模式实现

--- chan进阶使用/chan_or-done.go
+++ chan进阶使用/chan_or-done.go
@@ -1,8 +1,12 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
 
-// or or-done 模式
+// or or-done 模式 递归实现
 func or(channels ...<-chan interface{}) <-chan interface{} {
 	// 特殊情况，只有零个,1个或2个 chan
 	switch len(channels) {
@@ -20,7 +24,7 @@ func or(channels ...<-chan interface{}) <-chan interface{} {
 	orDone := make(chan interface{})
 	go func() {
 		defer close(orDone)
-
+		// fmt.Println("执行")
 		if len(channels) > 2 {
 			m := len(channels) / 2
 			select {
@@ -32,8 +36,8 @@ func or(channels ...<-chan interface{}) <-chan interface{} {
 	return orDone
 }
 
-func sig(after time.Duration) <-chan struct{} {
-	c := make(chan struct{})
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
 	go func() {
 		defer close(c)
 		time.Sleep(after)
@@ -42,6 +46,46 @@ func sig(after time.Duration) <-chan struct{} {
 	return c
 }
 
+// orSelect 反射⽅式 实现
+func orSelect(channels ...<-chan interface{}) <-chan interface{} {
+	// 特殊情况，只有零个,1个或2个 chan
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		// 利用反射构建SelectCase
+		var cases []reflect.SelectCase
+		for _, c := range channels {
+			cases = append(cases, reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(c),
+			})
+		}
+		// 随机选取一个可用 case
+		reflect.Select(cases)
+	}()
+
+	return orDone
+}
+
 func main() {
+	start := time.Now()
+
+	<-orSelect(
+		sig(10*time.Second),
+		sig(20*time.Second),
+		sig(30*time.Second),
+		sig(40*time.Second),
+		sig(50*time.Second),
+		sig(01*time.Second),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
 
 }
